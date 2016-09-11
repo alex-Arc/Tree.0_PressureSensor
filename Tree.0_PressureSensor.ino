51d96ccb1dace310404b17249013e738a6c520b5
alex-Arc <alex@arc-light.dk>
2016-09-11 17:14:32 +0200

    code outline

--- Tree.0_PressureSensor.ino
+++ Tree.0_PressureSensor.ino
@@ -1,9 +1,44 @@
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <SPI.h>
+#include <OSCBundle.h>
+#include <OSCTiming.h>
+
+EthernetUDP Udp;
+
+//Arduino IP
+IPAddress inIP(192.168.1.200);
+//host IP
+IPAddress outIP(192.168.1.2);
+
+//Arduino port
+uint16_t inPort = 49160;
+//defualt host port
+uint16_t outPort = 49155;
+
+//arduino mac addr.
+const char mac[] = {
+ 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+ const int sensorPin[4]  = {A0, A1, A2, A3};
+ float curentVal[4]  = {};
+ float activVal[4]   = {};
+
 void setup() {
-  // put your setup code here, to run once:
+  /////////DEBUG/////////
+  Serial.Begin(9600);
+  delay(500);
+  Serial.println("begin");
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //analogRead
+  //smooth                    or filter so mutch it only triggers true
+  // if dif > thres
+    //send OSC
+
 
 }
